a351c4c25de3adb93fa39a6fcef3c6b0ea594d2e
Fábio Moreira <fabiomsnet@gmail.com>
2018-07-10 22:29:33 -0300

    Create class Usuario

---
+++ src/main/java/br/com/fabiomsnet/model/Usuario.java
@@ -0,0 +1,89 @@
+package br.com.fabiomsnet.model;
+
+import java.util.Date;
+
+public class Usuario {
+
+	private int id;
+	private String primeiroNome;
+	private String ultimoNome;
+	private String email;
+	private String estado;
+	private String cidade;
+	private boolean ativo;
+	private boolean status;
+	private Date dataCadastro;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+}
